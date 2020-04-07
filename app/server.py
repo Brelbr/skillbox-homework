71d4dc883b6b8ff0eeb2915c636ad3ef59b773ff
brel <brelbr@gmail.com>
2020-04-08 02:20:11 +0300

    Выполнение задания

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 #
 # Серверное приложение для соединений
-# С изменениями по домашней работе
+# С изменениями по домашнему заданию
+#
 #
 import asyncio
 from asyncio import transports
@@ -15,43 +16,57 @@ class ServerProtocol(asyncio.Protocol):
         self.server = server
 
     def data_received(self, data: bytes):
-        print(data)
-
         decoded = data.decode()
-
         if self.login is not None:
             self.send_message(decoded)
         else:
             if decoded.startswith("login:"):
-                self.login = decoded.replace("login:", "").replace("\r\n", "")
+                offer_login = decoded.replace("login:", "").replace("\r\n", "")
+                for user in self.server.clients:
+                    if user.login == offer_login:
+                        self.transport.write(f"Логин {offer_login} занят, попробуйте другой!".encode())
+                        self.transport.close()
+                        return
+
+                self.login = offer_login
                 self.transport.write(
                     f"Привет, {self.login}!\n".encode()
                 )
+                self.send_history()
             else:
                 self.transport.write("Неправильный логин\n".encode())
 
     def connection_made(self, transport: transports.Transport):
         self.server.clients.append(self)
         self.transport = transport
-        print("Пришел новый клиент")
+        address = self.transport.get_extra_info("peername")     # Получаем адрес подключившегося
+        print(f"Пришел новый клиент {address}")
 
     def connection_lost(self, exception):
         self.server.clients.remove(self)
         print("Клиент вышел")
 
     def send_message(self, content: str):
-        message = f"{self.login}: {content}\n"
+        message = f"{self.login}: {content}"
+        print(f"{message}")
+        self.server.history.append(message)     # Заполненение истории
+        if len(self.server.history) > 10:       # Ограничение по количеству сообщений в истории
+            self.server.history.pop(0)
 
         for user in self.server.clients:
             user.transport.write(message.encode())
 
+    def send_history(self):     # Функция отправки истории
+        for message in self.server.history:
+            self.transport.write(message.encode())
 
 class Server:
     clients: list
+    history: list   # Добавляем список для сохранения истории сообщений
 
     def __init__(self):
         self.clients = []
-
+        self.history = []
     def build_protocol(self):
         return ServerProtocol(self)
 
